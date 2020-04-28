c2f5c32e5dae42675fb085fee903e5cd63c4be48
Jacob <Jtwalsh7@gmail.com>
2020-04-27 21:01:42 -0400

    added some methods, implemented default method for isEmpty()

--- JacobsCollection.java
+++ JacobsCollection.java
@@ -4,15 +4,58 @@
  *  @author Jacob Walsh
  */
 
-import java.util.Collection;
 interface JacobsCollection<junk> extends Iterable<junk>{
+
+	int size = 0;
 
 	/**
 	 *adds item to collection
-	 * @param item, generic item type to add
+	 * @param item, generic item to add
 	 * @return boolean verifying item was added
 	 */
 	boolean add(junk item);
 
+	/**
+	 *adds collection of items to this collection
+	 * @param items, collection of items to add
+	 * @return boolean verifying items were added
+	 */
+	boolean addAll(JacobsCollection<? extends junk> items);
 
+
+	/**
+	 *removes all items from collection
+	 */
+	void clear();
+
+	/**
+	 *checks if item is in collection
+	 * @param item, item being checked
+	 * @return boolean verifying item is contained
+	 */
+	boolean contains(junk item);
+
+	 /**
+	  *checks if collection of items are in this collection
+	  * @param items, items being checked
+	  * @return boolean verifying items are present
+	 */
+	boolean containsAll(JacobsCollection<?> items);
+
+	/**
+	  *checks if collection of items are "equal"
+	  * @param O Object being compared
+	  * @return boolean verifying items this and O are the same
+	 */
+	boolean equals(Object O);
+
+/**
+	  *Computes and returns collections hash code
+	  * @return hash code of this collection
+	 */
+	int hashCode();
+
+	default boolean isEmpty(){
+			return size==0;
+	}
 }
